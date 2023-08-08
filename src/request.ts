44b0cb36412eed80d3d609258d65e65381031238
Dafa Armanto <randikadafa07@gmail.com>
2023-08-09 06:44:03 +0700

    Batch commit

---
+++ src/request.ts
@@ -0,0 +1,30 @@
+const requests: Record<
+  string,
+  {
+    completion: string;
+    pending: boolean;
+  }
+> = {};
+
+export const getRequest = (requestId: any) => {
+  return requests[requestId];
+};
+
+export const startRequest = async (prompt: string) => {
+  const requestId = Math.random().toString(36).substring(2, 15);
+  requests[requestId] = {
+    completion: "",
+    pending: true,
+  };
+
+  const interval = setInterval(() => {
+    requests[requestId].completion += prompt + " ";
+
+    if (requests[requestId].completion.length > 100) {
+      clearInterval(interval);
+      requests[requestId].pending = false;
+    }
+  }, 100);
+
+  return requestId;
+};
